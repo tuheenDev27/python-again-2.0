473bb973eebbaa4f168211f3335c292415f0d727
tuheenDev27 <tuheen@mail.com>
2026-08-23 03:24:39 +0000

    create two string variable

--- basic.py
+++ basic.py
@@ -3,3 +3,7 @@ this time is all about final call from my side to become a devops engineer with 
 no bulsit just hard woke no ai for coding next 6 month after one  week small project 
 '''
 print("once again start python but this time have to complete the work")
+# this is first variable for my new start
+Devops = "i have to become a devops and kubernetes engineer with python and best project in next one year"
+name = "Tuheen Naha"
+print(Devops, name)
