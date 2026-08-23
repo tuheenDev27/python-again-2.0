e1755d018d1f2ae3d7d16215f0c53c657d92fc2e
tuheenDev27 <tuheen@mail.com>
2026-08-23 03:19:43 +0000

    create a folder where i practice python

---
+++ basic.py
@@ -0,0 +1,5 @@
+'''
+this time is all about final call from my side to become a devops engineer with python programer with out using ai next 1 year is all about that
+no bulsit just hard woke no ai for coding next 6 month after one  week small project 
+'''
+print("once again start python but this time have to complete the work")
