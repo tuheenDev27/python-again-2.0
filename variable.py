c5d93cdac0d4c6b09777ceede570d7b6218e2c90
tuheenDev27 <tuheen@mail.com>
2026-08-23 05:37:20 +0000

    create new file and practice and understand type of variable in python

---
+++ variable.py
@@ -0,0 +1,23 @@
+'''
+create a vriable and perform some operation and play with them and uderstand how thinks works
+'''
+x= 5
+y = x
+x = "tuhin"
+y = "devops "
+print(x,y)
+del x
+
+# swapping two varaibles
+a,b = 10, 20
+a,b = b,a
+print(a,b)
+
+# dynamic typing in variable
+x = 5
+x = "tuhin"
+print(x)
+
+# assing variables 
+a,b,c = 5, "tuhin", 21
+print(a,b,c)
