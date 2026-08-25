bea0374733b5bbc726cc24e51accdf04440e31a8
TuheenDev <tuheennaha27@outlook.com>
2026-08-25 23:12:32 +0530

    if else statment

--- statment.py
+++ statment.py
@@ -5,3 +5,12 @@ today we will explore  basic candition statment and write simple code
 age = 21
 if age > 27:
     print("save money and travel all over world ")
+
+'''
+next we explore if else statment and write simple code
+'''
+name = "unsnika"
+age = 25
+college = "Presidency College"
+res = if age == 25 and name == "ushnika" and college == "presidency college"
+print(res)
